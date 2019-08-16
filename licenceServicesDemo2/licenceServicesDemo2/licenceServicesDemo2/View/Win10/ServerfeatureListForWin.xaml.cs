3ee346912b20afc08e8595398f4c43ff81dd15a4
kartik009 <lakhanikartikb@outlook.com>
2019-08-16 12:16:41 +0000

    FeatureListForWin Butoon Problem

--- licenceServicesDemo2/licenceServicesDemo2/licenceServicesDemo2/View/Win10/ServerfeatureListForWin.xaml.cs
+++ licenceServicesDemo2/licenceServicesDemo2/licenceServicesDemo2/View/Win10/ServerfeatureListForWin.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using licenceServicesDemo2.Model;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +14,12 @@ namespace licenceServicesDemo2.View.Win10
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class ServerfeatureListForWin : ContentPage
     {
+
+
+        public ObservableCollection<Servers> Temp;
+
+        public ObservableCollection<Feature> fe;
+
         private GridUnitType _height = 0;
         public GridUnitType height
         {
@@ -24,21 +32,69 @@ namespace licenceServicesDemo2.View.Win10
         public ServerfeatureListForWin()
         {
             InitializeComponent();
+            fe = new ObservableCollection<Feature>();
 
-            sv_main.IsVisible = true;
+            fe.Add(new Feature() { FeatureName = "f2", Ver = (float)2.0, count = 1, Available = 1, ExperyDate = "Permanent" });
+
+            fe.Add(new Feature() { FeatureName = "f2", Ver = (float)1.0, count = 100, Available = 92, ExperyDate = "Permanent" });
+
+            fe.Add(new Feature() { FeatureName = "f3", Ver = (float)1.0, count = 10, Available = 5, ExperyDate = "2021-01-31" });
+
+
+            Temp = new ObservableCollection<Servers>();
+
+
+            Temp.Add(new Servers() { ServerID = 1, SeverName = "Microsoft Azure", IP = "10.100.12.1", Used = 15, Available = 52, features = fe.ToList() });
+            Temp.Add(new Servers() { ServerID = 2, SeverName = "AWS", IP = "10.100.12.2", Used = 25, Available = 42, features = fe.ToList() });
+            Temp.Add(new Servers() { ServerID = 3, SeverName = "G-Cloude", IP = "10.100.12.3", Used = 35, Available = 32 });
+
+            ServerListView.ItemsSource = Temp;
+
+            //sv_Main.HeightRequest
+            sv_Main.IsVisible = true;
             sv_content.IsVisible = false;
         }
 
-        private void Button_Clicked(object sender, EventArgs e)
+        //private void Button_Clicked(object sender, EventArgs e)
+        //{
+        //    sv_content.IsVisible = true;
+        //    _height = GridUnitType.Auto;
+        //}
+
+        //private void Close_Clicked(object sender, EventArgs e)
+        //{
+
+        //    sv_content.IsVisible = false;
+        //    _height = 0;
+
+        //}
+
+        private void ServerListView_ItemTapped(object sender, ItemTappedEventArgs e)
         {
+            var serv = e.Item as Servers;
+
+            var features = serv.features;
+
+            FeatureListView.ItemsSource = features;
+
             sv_content.IsVisible = true;
+
             _height = GridUnitType.Auto;
+
+
         }
 
-        private void Close_Clicked(object sender, EventArgs e)
+        private void FeatureListView_ItemTapped(object sender, ItemTappedEventArgs e)
         {
 
-            sv_content.IsVisible = false;
+        }
+
+        private void BT_Close_Clicked(object sender, EventArgs e)
+        {
+
+            sv_content.IsVisible = false;         
+            
+            sv_content.HeightRequest = 0;
             _height = 0;
 
         }
